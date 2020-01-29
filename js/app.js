1f298ea4ab28419eda0a5f6c4d516286b9bd8c47
Kristiana Blake <kristiana.blake@gmail.com>
2020-01-29 16:37:44 -0600

    fixed indentation errors and psuedo code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const checkPyramid = (str) => {
 
 
-	if (pyramid - return true) {
+	if (str.length < 0) {
 
 		// arrange the letters in increasing frequency starting with one 
 		// and continuing without gaps - don't count spaces 
@@ -17,36 +17,45 @@ const checkPyramid = (str) => {
 			if (str[i] != " "){
 				// creating new word with no spaces
 				word += str[i]
-				// rearrange the letters in a string in order of frequency (in variable word)
-				// and without duplicates: 
-				// if (word[i]) is falsey (undefined, 0, null) then the value of that index
-				// has been encountered and counted previously, otherwise, we initialize it 
-				if (word[i]) {
-					word[i]++;
-				} else {
-					word[i] = 1;
-				}
-
-				//make sure there are multiple numbers of that one index (not 1 b and 1 d )
-
-				return true  
-
 			} else {
 				// if the charcter does equal a space, it is pushed into the wordArr
 				wordArr.push(word)
 				// this is emptying out the variable "word"
-				word = "";
+				word = " ";
 			}
 
-			// this pushed the last word into wordArr
-			wordArr.push(word)
+		// this pushes the last letter from the word variable into
+		// variable wordArr
+		wordArr.push(word)
+		// checks the frequency of the letter
+		for (let i = 0; i < wordArr.length; i++) {
+			// rearrange the letters in a string in order of 
+			// frequency (in variable word)
+
+			//frequency:
+			// if (word[i]) is falsey (undefined, 0, null) 
+			//then the value of that index
+			// has been encountered and counted previously, otherwise, 
+			//we initialize it 
+
+			if (wordArr[i]) {
+				wordArr[i]++;
+			} else {
+				wordArr[i] = 1;
+			}
+
+			// and without duplicates: 
+			// duplicates:
+			// make sure there are multiple numbers of 
+			// that one index (not 1 b and 1 d )
+		}
 
 
 		}
 		
 
-	} else {
-	 	return false
+		} else {
+	 		return false
 	 } 
 
 } 
